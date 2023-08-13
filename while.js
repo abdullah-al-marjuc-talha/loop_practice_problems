9ec087a32d20e80c40fd5ef64f294aa48ea3940a
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-14 03:50:42 +0600

    whileloop added

---
+++ while.js
@@ -0,0 +1,14 @@
+// while.js
+
+var roastGiven = 0;
+// if (roastGiven < 7)
+// {
+
+// }
+
+while(roastGiven< 7)
+{
+    console.log('roast den, please!!')
+    console.log(roastGiven);
+    roastGiven++;
+}
