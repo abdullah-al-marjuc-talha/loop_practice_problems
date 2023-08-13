73179bbeb93a92f5c3b247e99b1019f0bca42d09
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-14 03:40:16 +0600

    revision added

--- revision.js
+++ revision.js
@@ -1,1 +1,32 @@
 // revision.js
+
+//string type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+//number type variable
+var sunglassprice =  120;
+var panColor = 'blue';
+//
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+
+var item3 = tableItems[3];
+tableItems[4] = 'notebook';
+var panIndex = tableItems.indexOf('pen');
+//puah/pop
+
+
+//conditionals
+if(tableItems.length > 4)
+{
+
+}
+else if (tableItems[3] == 'pen')
+{
+
+}
+else
+{
+    
+}
