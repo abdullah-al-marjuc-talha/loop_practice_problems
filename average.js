2e897c5a531d455958d7dd12470d3850519312b0
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-15 02:57:33 +0600

    average.js added

---
+++ average.js
@@ -0,0 +1,11 @@
+var mathMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+
+var totatMarks = mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
+console.log(totatMarks);
+var average = totatMarks / 5;
+var averageTeoDecimal = average.toFixed(2);
+console.log(averageTeoDecimal);
