c4ff9a78cdabeb23f9b5d0b6eaf843b93b62e989
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-15 02:17:40 +0600

    array loop added

---
+++ array-loop.js
@@ -0,0 +1,27 @@
+// for(var i =0; i<10; i++)
+// {
+//     console.log(i);
+// }
+
+// var numbers = [45,63,84,30,28,39,74];
+// for(var i=0; i<7; i++)
+// {
+//     var number = numbers[i];
+//     console.log(number);
+
+// }
+
+// var numbers = [45,63,84,30,28,39,74,32,21];
+// for(var i=0; i<numbers.length; i++)
+// {
+//     var number = numbers[i];
+//     console.log(number);
+
+// }
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen'];
+for(var i=0; i<items.length; i++)
+{
+    var item = items[i];
+    console.log(item)
+}
