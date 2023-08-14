2e4ff294c25d4f8d03c69370723762cc44bd0ff4
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-15 02:33:42 +0600

    continue added

---
+++ continue.js
@@ -0,0 +1,11 @@
+var numbers = [45,63,84,30,28,39,174,32,21];
+for(var i=0; i<numbers.length; i++)
+{
+    var number = numbers[i];
+    if(number>50)
+    {
+        continue;
+    }
+    console.log(number);
+
+}
